1753e180e4ba5e07fc30f70df782b3569a026818
marcosantonastasi <marco.santonastasi@gmail.com>
2023-03-09 21:04:10 +0100

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,9 @@
 </script>
 
 <h1>Ciao Svelte Society Italia</h1>
+
+<a href="/auth/signin" class="buttonPrimary" data-sveltekit-preload-data="off">Sign in</a>
+
 {#if $page.data}
 	<p>{JSON.stringify($page.data)}</p>
 {/if}
-{#if data}
-	<p>{JSON.stringify(data)}</p>
-{/if}
-
-<a href="/auth/signin" class="buttonPrimary" data-sveltekit-preload-data="off">Sign in</a>
